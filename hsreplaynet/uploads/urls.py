f1bdf8c1278a332e75714d1ea28892ac631bd4d1
Jerome Leclanche <jerome@leclan.ch>
2016-08-09 02:02:11 +0300

    Change upload failure UI url not to conflict

--- hsreplaynet/uploads/urls.py
+++ hsreplaynet/uploads/urls.py
@@ -3,7 +3,7 @@ from .views import UploadDetailView, list_upload_failures, reprocess_failed_uplo
 
 
 urlpatterns = [
-	url(r"^upload/failures/$", list_upload_failures, name="failures_list"),
-	url(r"^upload/failures/(?P<shortid>[\w-]+)/$", reprocess_failed_upload, name="reprocess_failed_upload"),
+	url(r"^failures/$", list_upload_failures, name="failures_list"),
+	url(r"^failures/(?P<shortid>[\w-]+)/$", reprocess_failed_upload, name="reprocess_failed_upload"),
 	url(r"^upload/(?P<shortid>[\w-]+)/$", UploadDetailView.as_view(), name="upload_detail"),
 ]
