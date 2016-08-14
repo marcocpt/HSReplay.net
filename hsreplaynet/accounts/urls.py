d50c055554199fd6ce45aec8734bb59181a9aa7f
Jerome Leclanche <jerome@leclan.ch>
2016-08-14 13:34:41 +0300

    Do not register account urls on Lambda either

--- hsreplaynet/accounts/urls.py
+++ hsreplaynet/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 from .views import ClaimAccountView, DeleteAccountView
 
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r"^$", TemplateView.as_view(template_name="account/edit.html"), name="account_edit"),
 	url(r"^claim/(?P<id>[\w-]+)/$", ClaimAccountView.as_view(), name="account_claim"),
 	url(r"^delete/$", DeleteAccountView.as_view(), name="account_delete"),
+	url(r"^", include("allauth.urls")),
 ]
--- hsreplaynet/urls.py
+++ hsreplaynet/urls.py
@@ -7,8 +7,6 @@ from .games.views import ReplayDetailView
 urlpatterns = [
 	url(r"^$", TemplateView.as_view(template_name="home.html"), name="home"),
 	url(r"^api/", include("hsreplaynet.api.urls")),
-	url(r"^account/", include("allauth.urls")),
-	url(r"^account/", include("hsreplaynet.accounts.urls")),
 	url(r"^games/", include("hsreplaynet.games.urls")),
 	url(r"^scenarios/", include("hsreplaynet.scenarios.urls")),
 	url(r"^uploads/", include("hsreplaynet.uploads.urls")),
@@ -22,6 +20,7 @@ if not settings.ENV_LAMBDA:
 	# Do not register admin/flatpages on Lambda as they are not installed
 	urlpatterns += [
 		url(r"^admin/", include("hsreplaynet.admin.urls")),
+		url(r"^account/", include("hsreplaynet.accounts.urls")),
 		url(r"^about/privacy/$", flatpage, {"url": "/about/privacy/"}, name="privacy_policy"),
 		url(r"^about/tos/$", flatpage, {"url": "/about/tos/"}, name="terms_of_service"),
 		url(r"^pages/", include("django.contrib.flatpages.urls")),
