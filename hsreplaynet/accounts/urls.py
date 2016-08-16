4782ee7153666c965bff517fd4db2c4b13e37dc6
Jerome Leclanche <jerome@leclan.ch>
2016-08-16 20:26:50 +0300

    Implement a way to make a social account "primary"
    Making a social account primary updates the user's username to that
    of that social account's battletag.
    
    Closes #53

--- hsreplaynet/accounts/urls.py
+++ hsreplaynet/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
-from .views import ClaimAccountView, DeleteAccountView
+from .views import ClaimAccountView, DeleteAccountView, MakePrimaryView
 
 
 urlpatterns = [
 	url(r"^$", TemplateView.as_view(template_name="account/edit.html"), name="account_edit"),
 	url(r"^claim/(?P<id>[\w-]+)/$", ClaimAccountView.as_view(), name="account_claim"),
 	url(r"^delete/$", DeleteAccountView.as_view(), name="account_delete"),
+	url(r"^make_primary/$", MakePrimaryView.as_view(), name="socialaccount_make_primary"),
 	url(r"^", include("allauth.urls")),
 ]
