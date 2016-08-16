beeb823ce0cbf940a061e98b41b528a3d15ad974
Jerome Leclanche <jerome@leclan.ch>
2016-08-16 20:54:25 +0300

    Require authentication for the edit account view

--- hsreplaynet/accounts/urls.py
+++ hsreplaynet/accounts/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
-from django.views.generic import TemplateView
-from .views import ClaimAccountView, DeleteAccountView, MakePrimaryView
+from .views import ClaimAccountView, DeleteAccountView, EditAccountView, MakePrimaryView
 
 
 urlpatterns = [
-	url(r"^$", TemplateView.as_view(template_name="account/edit.html"), name="account_edit"),
+	url(r"^$", EditAccountView.as_view(), name="account_edit"),
 	url(r"^claim/(?P<id>[\w-]+)/$", ClaimAccountView.as_view(), name="account_claim"),
 	url(r"^delete/$", DeleteAccountView.as_view(), name="account_delete"),
 	url(r"^make_primary/$", MakePrimaryView.as_view(), name="socialaccount_make_primary"),
