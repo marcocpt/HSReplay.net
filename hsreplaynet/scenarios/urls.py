629eb1c147f2180a4cf5f82e27efecafd0fac8b0
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-08-19 00:03:55 +0200

    Add list page for scenarios

--- hsreplaynet/scenarios/urls.py
+++ hsreplaynet/scenarios/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import ScenarioDetailsView
+from .views import ScenarioDetailsView, ScenarioListView
 
 
 urlpatterns = [
+	url(r"^$", ScenarioListView.as_view(), name="scenario_list_view"),
 	url(
 		r"^(?P<scenario_id>\d+)/$", ScenarioDetailsView.as_view(), name="scenario_details_view"
 	),
+
 ]
