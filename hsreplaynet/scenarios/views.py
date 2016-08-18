db8a632a6fd52c7041065a1bbebb6b05f0a431fd
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-08-19 00:03:55 +0200

    Add list page for scenarios

--- hsreplaynet/scenarios/views.py
+++ hsreplaynet/scenarios/views.py
@@ -3,6 +3,15 @@ from django.views.generic import View
 from .models import Scenario
 
 
+class ScenarioListView(View):
+	def get(self, request):
+		context = {
+			"scenarios": Scenario.objects.filter(adventure=10).all()
+		}
+
+		return render(request, "scenarios/scenarios_list.html", context)
+
+
 class ScenarioDetailsView(View):
 	def get(self, request, scenario_id):
 		context = {
