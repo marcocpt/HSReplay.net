165795d55ee937714998c939cc45920c8d95e729
Benedict Etzel <developer@beheh.de>
2016-08-20 02:49:31 +0200

    Fix missing font on Joust demo

--- hsreplaynet/templates/joust_include.js
+++ hsreplaynet/templates/joust_include.js
@@ -6,7 +6,7 @@ var joust_required = [
 	"{% joust_static 'joust.js' %}",
 	"{% static 'scripts/joust-extra.js' %}",
 	"{% static 'fonts/belwefs_extrabold_macroman/stylesheet.css' %}",
-	"{% static 'fonts/belwefs_extrabold_macroman/stylesheet.css' %}",
+	"{% static 'fonts/franklingothicfs_mediumcondensed_macroman/stylesheet.css' %}",
 ];
 
 $(document).ready(function() {
