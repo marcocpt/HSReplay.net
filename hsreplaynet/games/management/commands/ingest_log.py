477e8b5e278cac6fbcfeb5a377fd0df6cbffb5c8
Jerome Leclanche <jerome@leclan.ch>
2016-09-06 12:54:15 +0300

    Implement --username option to ingest_log

--- hsreplaynet/games/management/commands/ingest_log.py
+++ hsreplaynet/games/management/commands/ingest_log.py
@@ -2,14 +2,25 @@ import json
 from django.core.files import File
 from django.core.management.base import BaseCommand
 from django.utils.timezone import now
+from hsreplaynet.accounts.models import User
 from hsreplaynet.uploads.models import UploadEvent
 
 
 class Command(BaseCommand):
 	def add_arguments(self, parser):
 		parser.add_argument("file", nargs="+")
+		parser.add_argument(
+			"--username", nargs="?", type=str,
+			help="User to attach the resulting replays to"
+		)
 
 	def handle(self, *args, **options):
+		username = options["username"]
+		if username:
+			user = User.objects.get(username=username)
+		else:
+			user = None
+
 		for file in options["file"]:
 			print("Uploading %r" % (file))
 			metadata = {
@@ -31,6 +42,9 @@ class Command(BaseCommand):
 
 			event.process()
 			self.stdout.write("%r: %s" % (event, event.get_absolute_url()))
+
 			if event.game:
+				event.game.user = user
+				event.game.save()
 				self.stdout.write("%r: %s" % (event.game, event.game.get_absolute_url()))
 				self.stdout.write("Replay: %s" % (event.game.replay_xml.url))
