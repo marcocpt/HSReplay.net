f668faf1017fd4594fc443e6801be8272c5e6fdb
Jerome Leclanche <jerome@leclan.ch>
2016-09-10 20:49:04 +0300

    Start rqworker on Vagrant

--- scripts/provision_run.sh
+++ scripts/provision_run.sh
@@ -2,19 +2,22 @@ PROJECT="$HOME/hsreplay.net"
 source "$HOME/env/bin/activate"
 export PATH="$VIRTUAL_ENV/nodeenv/bin:$HOME/node_modules/.bin:$PATH"
 
+# Kill remnants
+killall -9 -q python node sassc
+
 echo "Starting webpack watcher"
-killall -9 -q node
 webpack --verbose -d \
 	--devtool cheap-module-eval-source-map \
 	--config "$PROJECT/webpack.config.js" \
 	--watch &
 
 echo "Starting scss watcher"
-killall -9 -q sassc
 sassc "$PROJECT/hsreplaynet/static/styles/main.scss" "$PROJECT/hsreplaynet/static/styles/main.css" \
 	--sourcemap --source-comments \
 	--watch &
 
+echo "Starting RQ Workers"
+python "$PROJECT/manage.py" rqworker &
+
 echo "Starting Django server"
-killall -9 -q python
 python "$PROJECT/manage.py" runserver 0.0.0.0:8000
