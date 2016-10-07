38060261fe3cb849951451b810c498fdf4e065df
Jerome Leclanche <jerome@leclan.ch>
2016-10-07 14:13:53 +0300

    Finish the Pack write API

--- hsreplaynet/packs/api.py
+++ hsreplaynet/packs/api.py
@@ -1,19 +1,47 @@
+from rest_framework import serializers
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import GenericViewSet
 from hsreplaynet.api.authentication import AuthTokenAuthentication, RequireAuthToken
 from hsreplaynet.api.permissions import IsOwnerOrStaff, APIKeyPermission
-from hsreplaynet.api.serializers import UserSerializer
-from .models import Pack
+from .models import Pack, PackCard
 
 
-class PackSerializer(HyperlinkedModelSerializer):
-	user = UserSerializer()
+CARDS_PER_PACK = 5
+
+
+class PackCardSerializer(serializers.ModelSerializer):
+	premium = serializers.BooleanField(default=False)
+
+	class Meta:
+		fields = ("card", "premium")
+		model = PackCard
+
+
+class PackSerializer(serializers.HyperlinkedModelSerializer):
+	id = serializers.IntegerField(read_only=True)
+	account_hi = serializers.IntegerField(min_value=1)
+	account_lo = serializers.IntegerField(min_value=1)
+	cards = PackCardSerializer(many=True, write_only=True)
 
 	class Meta:
 		model = Pack
-		fields = ("user", "booster_type", "date", "account_hi", "account_lo")
+		fields = (
+			"id", "booster_type", "date", "account_hi", "account_lo", "cards"
+		)
+
+	def create(self, validated_data):
+		validated_data["user"] = self.context["request"].user
+		cards = validated_data.pop("cards")
+		if len(cards) != CARDS_PER_PACK:
+			raise ValidationError("Packs must contain exactly %r cards" % (CARDS_PER_PACK))
+		pack = Pack.objects.create(**validated_data)
+
+		for card in cards:
+			PackCard.objects.create(pack=pack, card=card["card"], premium=card["premium"])
+
+		return pack
 
 
 class PackViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
